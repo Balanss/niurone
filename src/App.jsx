1d111119d55bfed0e4d56a73f4a37b8b1a59aab2
gilmon <gilmonpurry@hotmail.com>
2024-03-19 17:36:41 -0300

    Refactor scroll handling and background styling

--- src/App.jsx
+++ src/App.jsx
@@ -39,29 +39,14 @@ useEffect(() => {
   window.scrollTo(0, 0);
 }, []);
 
-
-
-useEffect(() => {
-  const handleScroll = () => {
-    const scrollHeight = window.innerHeight;
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition >= scrollHeight * 0.6) {
-      setBgTheme('2');
-    } else if (scrollPosition >= scrollHeight * 0.25) {
-      setBgTheme('1');
-    }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
+<<<<<<< HEAD
+=======
 
 
 
+
+
+>>>>>>> 444c3643c3bbc3b3377abc10637858042d6d682b
 useEffect(() => {
   setTimeout(() => {
     setContactSide(true);
@@ -75,31 +60,51 @@ const { scrollYProgress } = useViewportScroll();
 const opacity = useTransform(scrollYProgress, [0.9, 1, 1], [0.9, 1, 1]);
 const scale = useTransform(scrollYProgress, [1, 1, 1], [0, 1, 1]);
 
-//bg-new
+
+
+const [changeBg, setChangeBg] = useState('newBg');
+
+
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> 444c3643c3bbc3b3377abc10637858042d6d682b
+const gradients = {
+  newBg: 'radial-gradient(rgb(55, 65, 81), rgb(17, 24, 39), rgb(0, 0, 0))',
+  new: 'radial-gradient(rgb(29, 34, 43), rgb(18, 22, 33), rgb(39, 50, 67))'
+};
+
 
   return (
    
       <BrowserRouter>
-   <motion.div className={`  relative inset-0  z-50  bg-cover bg-no-repeat  `}  initial={{ opacity: 0.9 }}
-                     animate={{ opacity: 1 }}
-                     transition={{ duration: 0.2 ,ease:'easeInOut'}}>
-        <Navbar status={status} setStatus={setStatus}/>
-        
-        <LazyLoadImage
-  alt={'background image'}
-  src={bgImage} // replace with the path to your image
-  className={` w-screen h-screen fixed `}
-/> 
-        <div  className=" "  >
-          <LandingPage status={status} setStatus={setStatus} />
-        </div>
+<<<<<<< HEAD
+  <motion.div className={`relative inset-0 z-50`} initial={{ opacity: 0.9 }} style={window.innerWidth < 625?{ backgroundImage: gradients['new'] } : { backgroundImage: gradients['newBg'] }}
+=======
+  <motion.div className={`relative inset-0 z-50`} initial={{ opacity: 0.9 }} style={{ backgroundImage: gradients[changeBg] }}
+>>>>>>> 444c3643c3bbc3b3377abc10637858042d6d682b
+    animate={{ opacity: 1 }}
+    transition={{ duration: 0.2 ,ease:'easeInOut'}}>
+    <Navbar status={status} setStatus={setStatus}/>
+    
+    <div>
+      <LandingPage status={status} setStatus={setStatus} />
+    </div>
 
     <section className="relative overflow-hidden" >
       
-       <ServiceSuit />
-       
-          <MiddleText/>
-        </motion.div>
+<<<<<<< HEAD
+      <Suspense fallback={<div>Loading...</div>}>
+      <SparklesCore id="sparkles" className="absolute background top-10 hidden  tablet:flex  tablet:right-8 lg:right-20  xl:right-40 bottom-10 z-[-1] w-[10%]" />
+      </Suspense>
+=======
+      <SparklesCore id="sparkles" className="absolute background top-10 hidden  tablet:flex  tablet:right-8 lg:right-20  xl:right-40 bottom-10 z-[-1] w-[10%]" />
+>>>>>>> 444c3643c3bbc3b3377abc10637858042d6d682b
+      <motion.div style={{ opacity, scale }}>
+        <ServiceSuit />
+        <MiddleText/>
+      </motion.div>
 
       <div className="relative z-[10000] "  >
         <Suspense fallback={<div>Loading...</div>}>
